633f90f2f30161a3c463c4f582372121b824d27b
Chevy Ray Johnston <happytrash@gmail.com>
2022-11-30 21:40:13 -0800

    day 1 solutions

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,8 +1,39 @@
 fn main() {
-
+    let input = include_str!("day1.txt");
+    println!("PART 1: {}", solve_part1(input));
+    println!("PART 2: {}", solve_part2(input));
 }
 
 #[test]
 fn test() {
+    let test_input = include_str!("day1_test.txt");
+    assert_eq!(solve_part1(test_input), 24000);
+    assert_eq!(solve_part2(test_input), 45000);
+}
 
+fn solve_part1(input: &str) -> i32 {
+    input
+        .split("\n\n")
+        .map(|items| {
+            items
+                .lines()
+                .map(|calories| calories.parse::<i32>().unwrap())
+                .sum::<i32>()
+        })
+        .max()
+        .unwrap_or(0)
+}
+
+fn solve_part2(input: &str) -> i32 {
+    let mut max_for_each: Vec<i32> = input
+        .split("\n\n")
+        .map(|items| {
+            items
+                .lines()
+                .map(|item| item.parse::<i32>().unwrap())
+                .sum::<i32>()
+        })
+        .collect();
+    max_for_each.sort();
+    max_for_each.iter().rev().take(3).sum::<i32>()
 }
